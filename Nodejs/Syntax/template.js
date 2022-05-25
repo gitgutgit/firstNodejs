0866745b78f1f329a557affd1e2a6ce6ab89b6ea
gitgutgit <105686439+gitgutgit@users.noreply.github.com>
2022-05-25 12:10:33 +0900

    Node js Url..

---
+++ Nodejs/Syntax/template.js
@@ -0,0 +1,7 @@
+var name = 'jee'
+var letter = 'Dear ' +name+'\n\nasdsadsweradsadsadsas asdsadsa asdsad! ' +name+ ' done';
+
+var letter = `Dear ${name}
+
+asdsadsweradsadsadsas ${1+1} asdsadsa asdsad!${name} done`;
+console.log(letter);
